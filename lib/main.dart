f4f70861383dbc84ad8b056c7a436977381d32fc
Godawela <godawelaus.21@uom.lk>
2024-12-06 18:01:12 +0530

    Final

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,12 @@
-import "dart:io";
+import 'dart:io';
+import 'package:flutter/material.dart';
+import 'package:flutter_drawing_board/flutter_drawing_board.dart';
 
-import "package:flutter/material.dart";
-import "package:flutter_drawing_board/flutter_drawing_board.dart";
-
-main() {
+void main() {
   runApp(const MaterialApp(
     debugShowCheckedModeBanner: false,
     home: DrawingApp(),
-  ));  
+  ));
 }
 
 class DrawingApp extends StatefulWidget {
@@ -19,41 +18,55 @@ class DrawingApp extends StatefulWidget {
 
 class _DrawingAppState extends State<DrawingApp> {
   late DrawingController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = DrawingController();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.blue,
         centerTitle: true,
-        title:const Text("Drawin App",
-          style: TextStyle(color: Colors.white,fontWeight: FontWeight.bold),
+        title: const Text(
+          "Drawig App",
+          style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
         ),
       ),
-
       body: DrawingBoard(
-
-        controller:_controller ,
-
+        controller: _controller,
         background: Container(
         color: Colors.white,
         height: 600,
         width: 600,
       ),
-      showDefaultActions: true,
-      showDefaultTools: true,
+        showDefaultActions: true,
+        showDefaultTools: true,
       ),
-      floatingActionButton: FloatingActionButton(onPressed: saveDrawing,
-      backgroundColor: Colors.blue,
-      child: const Icon(Icons.save_alt,color: Colors.white,),
-      )
-      
+      floatingActionButton: FloatingActionButton(
+        onPressed: saveDrawing,
+        backgroundColor: Colors.blue,
+        child: const Icon(
+          Icons.save_alt,
+          color: Colors.white,
+        ),
+      ),
     );
   }
 
-  Future<void> saveDrawing()async{
-    try{
-      final imageData=await _controller.getImageData();
-      if(imageData==Null){
+  Future<void> saveDrawing() async {
+    try {
+      final imageData = await _controller.getImageData();
+      if (imageData == null) {
         print("No image to save");
         return;
       }
@@ -66,13 +79,12 @@ class _DrawingAppState extends State<DrawingApp> {
 
       path="${directory.path}/drawing.png";
 
-      final file=File(path);
-      await file.writeAsBytes(imageData.buffer.asInt8List());
+      final file = File(path);
+      await file.writeAsBytes(imageData.buffer.asUint8List());
       print("Drawing saved at $path");
-    }
 
-    catch(error){
-      print("Error $error");
+    } catch (error) {
+      print("Error: $error");
     }
   }
 }
